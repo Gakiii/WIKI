2edf9a643bbdef3834c23e315b7431c6391b2778
Jiacheng Hu <hujiacheng@cug.edu.cn>
2023-12-01 08:55:05 +0000

    a

--- download.py
+++ download.py
@@ -1,3 +1,3 @@
 from huggingface_hub import snapshot_download
 
-snapshot_download(repo_id="bert-base-uncased", allow_patterns=["*.json", "pytorch_model.bin", "vocab.txt"], local_dir="./my_model1/")
+snapshot_download(repo_id="bert-base-uncased", allow_patterns=["*.*"], local_dir="./my_model2/")
